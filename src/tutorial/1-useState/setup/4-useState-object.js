f0746cfca65ed3338e56ce7499b950c631b213f6
Manish <manisksing@gmail.com>
2022-10-02 21:57:09 +0530

    UseState - Multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  const [person, setPerson] = useState({
-    name:'manish',
-    age: 25, 
-    message: 'random message'
-  });
+  // const [person, setPerson] = useState({
+  //   name:'manish',
+  //   age: 25, 
+  //   message: 'random message'
+  // });
   //console.log(person);
+
+  const [name, setname] = useState('useState for name');
+  const [age, setage] = useState(24);
+  const [message, setmessage] = useState('useState for message');
 
   const changeMessage = () => {
     //setPerson('message updated ...');   // can't since its a string //
-    setPerson({...person, message:'message updated '});
+    //setPerson({...person, message:'message updated '});
+    if(age === 25) {
+      setname('useState for name');
+      setage(24);
+      setmessage('useState for message');
+    }
+    else {
+      setname('Manish Singh');
+      setage(25);
+      setmessage('Lives to Sleep');
+    }
+
   } 
   return (
   <>
-    <h3>{person.name}</h3>
-    <h3>{person.age}</h3>
-    <h3>{person.message}</h3>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
     <button className='btn' onClick={() => changeMessage()}>change Message</button>
   </>
   );
