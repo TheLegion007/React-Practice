193a4206a283f5d49a3ed6e1b919c93c9c32ef1c
Manish <manisksing@gmail.com>
2022-10-05 17:44:35 +0530

    Functional Update Form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,6 +6,14 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   }
+  const complexIncrease = () => {
+    setTimeout(()=>{
+//    setValue(value + 1)
+      setValue((preValue)=> {
+        return preValue + 1;
+      });
+    }, 2000);                                     // callback function , setValue func. is asynchronous , so passing paraameter to update counter even if we click simulataneously //
+  }
   return (
     <>
         <section style={{margin: '4rem 4rem', color: 'red'}}>
@@ -14,6 +22,11 @@ const UseStateCounter = () => {
           <button className='btn' onClick={()=> setValue(value-1)}>Decrease Count</button>
           <button className='btn' onClick={reset}>Reset Count</button>
           <button className='btn' onClick={()=> setValue(value+1)}>Increase Count</button>
+        </section>
+        <section style={{margin: '4rem 4rem', color: 'red'}}>
+          <h2>More Complex Counter</h2>
+          <h2>{value}</h2>
+          <button className='btn' onClick={complexIncrease}>Increase Later</button>
         </section>
     </>
   ); 
