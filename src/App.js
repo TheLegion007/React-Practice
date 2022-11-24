0a412a777fac8d8b5b5324d7c411796894a28afd
Manish <manisksing@gmail.com>
2022-11-24 23:42:28 +0530

    1-useEffect-basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-// import Setup from './tutorial/1-useState/setup/1-error-example.js'
-// import Setup from './tutorial/1-useState/setup/2-useState-basics'
-//import Setup from './tutorial/1-useState/setup/3-useState-array.js'
-//import Setup from './tutorial/1-useState/setup/4-useState-object.js'
-import Setup from './tutorial/1-useState/setup/5-useState-counter.js'
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
   return (
